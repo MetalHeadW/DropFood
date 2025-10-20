6f017ed92f658829b85317f76cabeac8615ec10b
joao_m_lessa <joao_m_lessa@estudante.sc.senai.br>
2025-10-20 16:44:04 -0300

    EmpresaModel 1.0

--- src/main/java/com/dropfood/model/EmpresaModel.java
+++ src/main/java/com/dropfood/model/EmpresaModel.java
@@ -14,18 +14,22 @@ import lombok.NoArgsConstructor;
 
 public class EmpresaModel {
     @Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-@Column(name = "IDEMPRESA")
-private int idEmpresa;
-    @Column(name = "NMEMPRESA")
-    private String nmEmpresa;
-    @Column(name = "DSEMPRESA")
-    private String dsEmpresa;
-    @Column(name = "TELEFONEEMPRESA")
-    private String telefoneEmpresa;
-    @Column(name = "CNPJ")
-    private String cnpjEmpresa;
-    @Column (name ="TPEMPRESA")
-    private String tipoEmpresa;
-}
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "IDEMPRESA")
+    private Integer id_empresa;
 
+    @Column(name = "NMEMPRESA", length = 100,  nullable = false)
+    private String nm_empresa;
+
+    @Column(name = "ENDERECO", length = 200,  nullable = false)
+    private String endereco_empresa;
+
+    @Column(name = "TELEFONEEMPRESA", length = 20,  nullable = false)
+    private String telefone_empresa;
+
+    @Column(name = "CNPJ", length = 18,  nullable = false)
+    private String cnpj_empresa;
+
+    @Column (name ="TPEMPRESA")
+    private String tipo_empresa;
+}
