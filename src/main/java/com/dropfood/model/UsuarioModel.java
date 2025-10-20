d2376195c1bba7bdebf13b4b08f1e1374b882d6d
joao_m_lessa <joao_m_lessa@estudante.sc.senai.br>
2025-10-20 13:41:40 -0300

    UsuarioModel

--- src/main/java/com/dropfood/model/UsuarioModel.java
+++ src/main/java/com/dropfood/model/UsuarioModel.java
@@ -14,13 +14,35 @@ public class UsuarioModel {
 @GeneratedValue(strategy = GenerationType.IDENTITY)
 @Column(name = "CDUSUARIO")
 private int cdCliente;
-    @Column(name = "NMUSUARIO")
-    private String nmCliente;
-    @Column(name = "DSCLIENTE")
-    private String dsCliente;
-    @Column(name = "VLSALARIOCLIENTE")
-    private Double vlSalarioCliente;
-    @Column(name = "FLNUMEROCLIENTE")
-    private String flNumeroCliente;
-}
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "IDUSUARIO")
+    private Integer id_Usuario;
+
+    @Column(name = "NMUSUARIO", length=100, nullable = false)
+    private String nm_Usuario;
+
+    @Column(name = "EMAIL",  length=100)
+    private String email;
+
+    @Column(name = "SENHAUSUARIO", length=50, nullable = false)
+    private String senha_Usuario;
+
+    @Column(name = "TELEFONE", length =20)
+    private String telefone;
+
+    @Column(name = "TIPUSUARIO", length =1, nullable = false)
+    private Integer tip_Usuario; // 1-Administrador. 2-Funcionario 3-Entregador. 4-Cliente.
+
+    @Column(name = "CPF", length =20, nullable = false)
+    private String cpf;
+
+    @Column(name = "ENDERECO", length=200)
+    private String endereco;
+
+    @Column(name = "PEFERENCIA1", length=30)
+    private String peferencia1;
+
+    @Column(name = "PEFERENCIA2", length=30)
+    private String peferencia2;
 }
